6e11f4a82060ab42a282416e9a825ac1cbcb727b
Vladimir <v.gorobets77@mail.ru>
2022-06-06 09:33:59 +0300

    добавил нормализацию

--- Rus2vec_my_models.py
+++ Rus2vec_my_models.py
@@ -1,5 +1,6 @@
 import gensim
-from gensim.models import Word2Vec, KeyedVectors
+from gensim.models import Word2Vec
+import normalization_words
 
 # import logging
 # import sys
@@ -17,13 +18,19 @@ from gensim.models import Word2Vec, KeyedVectors
 # model = gensim.models.KeyedVectors.load_word2vec_format('214/model.model', binary=False)
 
 # with open('214/model.model', 'r') as f:
-model = gensim.models.KeyedVectors.load('214/model.model')
+
+model = gensim.models.KeyedVectors.load('Rus2Vec_models/214/model.model')
 
 # Модели fasttext в новой версии gensim загружаются при помощи следующей команды::
 # gensim.models.KeyedVectors.load("model.model")
-# Перед загрузкой скачанный архив с моделью fasttext необходимо распаковать. Определить необходимый для загрузки файл несложно, чаще всего это файл с расширением .model (остальные файлы из архива должны быть в той же папке).
+
+# Перед загрузкой скачанный архив с моделью fasttext необходимо распаковать. Определить необходимый для загрузки файл
+# несложно, чаще всего это файл с расширением .model (остальные файлы из архива должны быть в той же папке).
 # Вернемся к нашей модели, созданной на основе НКРЯ. Скажем, нам интересны такие слова (пример для русского языка):
-words = ['день_NOUN', 'ночь_NOUN', 'человек_NOUN', 'семантика_NOUN', 'студент_NOUN', 'студент_ADJ']
+
+#words = ['день_NOUN', 'ночь_NOUN', 'человек_NOUN', 'семантика_NOUN', 'студент_NOUN', 'студент_ADJ']
+words = normalization_words.input_word(input())
+
 
 # Попросим у модели 10 ближайших соседей для каждого слова и коэффициент косинусной близости для каждого:
 for word in words:
@@ -40,10 +47,10 @@ for word in words:
         print(word + ' is not present in the model')
 
 # Находим косинусную близость пары слов:
-print(model.similarity('человек_NOUN', 'обезьяна_NOUN'))
+# print(model.similarity('человек_NOUN', 'обезьяна_NOUN'))
 
 # Найди лишнее!
-print(model.doesnt_match('яблоко_NOUN груша_NOUN виноград_NOUN банан_NOUN лимон_NOUN картофель_NOUN'.split()))
+# print(model.doesnt_match('яблоко_NOUN груша_NOUN виноград_NOUN банан_NOUN лимон_NOUN картофель_NOUN'.split()))
 
 # Реши пропорцию!
-print(model.most_similar(positive=['пицца_NOUN', 'россия_NOUN'], negative=['италия_NOUN'])[0][0])
+# print(model.most_similar(positive=['пицца_NOUN', 'россия_NOUN'], negative=['италия_NOUN'])[0][0])
